4aad004770431ccfdd47035f23b71d173fdb3260
shimla-akter <shimla4720@gmail.com>
2023-11-28 17:10:08 +0600

    first commit

---
+++ arbovirus/src/App.jsx
@@ -0,0 +1,18 @@
+import Button from "./components/Button"
+function App() {
+
+
+  return (
+    <>
+
+<Button text="Buy Now"/>
+<Button text="Log in"/>
+<Button text="Registration"/>
+<Button text="contact"/>
+
+
+    </>
+  )
+}
+
+export default App
